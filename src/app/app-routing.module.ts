f577294d9ce319909283672b443fcaaba980ad79
Adithya <adithya@cloud-9-solutions.com>
2019-09-30 13:47:39 +0530

    routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { ContainerComponent } from './container/container.component';
 
 
-const routes: Routes = [{path: 'user/details/:id', component : UserDetailsComponent}];
+const routes: Routes = [{path : '' , component: ContainerComponent},
+  {path: 'user/details/:id', component : UserDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
